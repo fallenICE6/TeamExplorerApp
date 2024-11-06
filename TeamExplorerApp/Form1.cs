edfc69aa68b47ed8341737aa9a0c53501252a712
matvey <matvey223225@mail.ru>
2024-11-06 08:58:41 +0300

    test

--- TeamExplorerApp/Form1.cs
+++ TeamExplorerApp/Form1.cs
@@ -16,5 +16,16 @@ namespace TeamExplorerApp
         {
             InitializeComponent();
         }
+        string text = "TESTING...";
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = null;
+            textBox1.Text = text;
+        }
     }
 }
